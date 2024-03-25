095307543396d3ccca7525fdaf0d1b12845b51eb
itzel <itzelalwarafii@gmail.com>
2024-03-25 11:15:48 -0700

    client set up

---
+++ Client/src/componets/Header.jsx
@@ -0,0 +1,8 @@
+
+
+export default function Header(){
+    return(
+
+        <h1> Im Hearder</h1>
+    )
+}
---
+++ Client/src/componets/Main/Event.jsx
@@ -0,0 +1,6 @@
+export default function Event(){
+    return(
+
+        <h1> Im Event</h1>
+    )
+}
