8c866a5ba4af34f0a6fcb4b96ccf30f59761faff
cys2110 <csheridan2110@gmail.com>
2024-03-25 18:57:05 -0400

    axios calls completed

--- Client/src/componets/Main/EventDetails.jsx
+++ Client/src/componets/Main/EventDetails.jsx
@@ -1,6 +1,77 @@
-export default function EventDetails(){
-    return(
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router'
 
-        <h1> Im EventDetails</h1>
-    )
+export default function EventDetails(){
+    
+    const { id } = useParams()
+    const [event, setEvent] = useState()
+    const [venue, setVenue] = useState()
+    const [date, setDate] = useState()
+
+    const convertTime = (timeString) => {
+        const [hours, minutes, seconds] = timeString.split(':').map(Number)
+        const period = hours >= 12 ? 'pm': 'am'
+        const hours12 = hours % 12 || 12
+        const formattedTime = `${hours12}:${String(minutes).padStart(2, '0')} ${period}`
+        return formattedTime
+    }
+
+    const capitaliseFirstLetter = (word) => {
+        return word.charAt(0).toUpperCase() + word.slice(1)
+    }
+
+    useEffect(() => {
+        const getEvent = async() => {
+            const response = await axios.get(`http://localhost:8000/events/${id}`)
+            setEvent(response.data)
+        }
+        getEvent()
+    }, [])
+
+    useEffect(() => {
+        const getVenue = async() => {
+            const response = await axios.get(event.venue)
+            setVenue(response.data)
+        }
+        const dateOptions = {
+            day: "numeric",
+            month: "long",
+            year: "numeric"
+        }
+        if (event) {
+            getVenue()
+            const formattedDate = new Date(event.date)
+            const localeDate = formattedDate.toLocaleDateString("en-US", dateOptions)
+            setDate(localeDate)
+        }
+    }, [event])
+
+    if (!event || !venue) {
+        return <h1>Loading...</h1>
+    } else {
+        return (
+            <div>
+                <h1>{event.name}</h1>
+                <img alt={event.name} src={event.image_url} />
+                <div>{capitaliseFirstLetter(event.venue_type)}</div>
+                <div>{venue.name}</div>
+                <div>{date}</div>
+
+                <dl>
+                    <dt>Start time</dt>
+                    <dd>{convertTime(event.start_time)}</dd>
+
+                    { event.door_time ? <dt>Doors open</dt> : null}
+                    { event.door_time ? <dd>{convertTime(event.door_time)}</dd> : null}
+
+                    <dt>General Admission tickets</dt>
+                    <dd>${event.ga_price}</dd>
+
+                    { event.vip_price ? <dt>VIP tickets</dt> : null }
+                    { event.vip_price ? <dd>${event.vip_price}</dd> : null }
+                </dl>
+            </div>
+        )
+    }
 }
--- Client/src/componets/Main/Venues.jsx
+++ Client/src/componets/Main/Venues.jsx
@@ -1,6 +1,41 @@
-export default function Venues(){
-    return(
+import { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
 
-        <h1> Im Venues</h1>
-    )
+export default function Venues(){
+
+    const [venues, setVenues] = useState([])
+
+    useEffect(() => {
+        const getVenues = async() => {
+            const response = await axios.get('http://localhost:8000/venues')
+            setVenues(response.data)
+        }
+        getVenues()
+    }, [])
+
+    const navigate = useNavigate()
+
+    const showVenue = (venue) => {
+        navigate(`/venues/${venue}`)
+    }
+
+    if (!venues) {
+        return <h1>Getting venues...</h1>
+    } else {
+        return (
+            <div>
+                <h1>Venues</h1>
+                <div className='venues-wrapper'>
+                    {venues.map(venue => (
+                        <div className='venue-card' key={venue.id} onClick={() => showVenue(venue.id)} >
+                            <img alt={venue.name} src={venue.image_url} />
+                            <h3>{venue.name}</h3>
+                            <h5>{venue.address}</h5>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        )
+    }
 }
